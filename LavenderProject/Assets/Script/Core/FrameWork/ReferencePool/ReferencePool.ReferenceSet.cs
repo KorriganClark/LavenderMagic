41fd275f437adfbedbce24aeb00637195a42f5f0
八佾舞雩 <852515440@qq.com>
2022-01-26 14:02:43 +0800

    引用池

---
+++ LavenderProject/Assets/Script/Core/FrameWork/ReferencePool/ReferencePool.ReferenceSet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lavender.FrameWork
+{
+    /// <summary>
+    /// 引用池
+    /// </summary>
+    public static partial class ReferencePool
+    {
+        private sealed class ReferenceSet
+        {
+            private readonly Queue<IReference> references;
+            private readonly Type referenceType;
+
+            private int usingReferenceCount;
+
+            public ReferenceSet(Type type)
+            {
+                references = new Queue<IReference>();
+                referenceType = type;
+                usingReferenceCount = 0;
+            }
+
+            public int UsingReferenceCount
+            {
+                get
+                {
+                    return usingReferenceCount;
+                }
+            }
+
+            public Type ReferenceType
+            {
+                get
+                {
+                    return referenceType;
+                }
+            }
+
+            public T Acquire<T>() where T: class, IReference, new()
+            {
+                if (typeof(T) != referenceType)
+                {
+                    throw new Exception("Type Not Match!");
+                }
+                usingReferenceCount++;
+                if(references.Count > 0)
+                {
+                    return (T)references.Dequeue();
+                }
+                return new T();
+            }
+
+        }
+    }
+}
--- LavenderProject/Assets/Script/Core/FrameWork/ReferencePool/ReferencePool.cs
+++ LavenderProject/Assets/Script/Core/FrameWork/ReferencePool/ReferencePool.cs
@@ -6,8 +6,44 @@ using System.Threading.Tasks;
 
 namespace Lavender.FrameWork
 {
-    class ReferencePool
+    /// <summary>
+    /// 引用池
+    /// </summary>
+    public static partial class ReferencePool
     {
+        private static readonly Dictionary<Type, ReferenceSet> referenceSets = new Dictionary<Type, ReferenceSet>();
+
+        public static int Count
+        {
+            get
+            {
+                return referenceSets.Count;
+            }
+        }
+
+        public static T Acquire<T>() where T : class, IReference, new()
+        {
+            
+        }
+
+        private static ReferenceSet GetReferenceSet(Type referenceType)
+        {
+            if (referenceType == null)
+            {
+                throw new Exception("RefereceType is null!");
+            }
+            ReferenceSet referenceSet = null;
+            lock (referenceSets)
+            {
+                if (!referenceSets.TryGetValue(referenceType, out referenceSet))
+                {
+                    referenceSet = new ReferenceSet();
+                    referenceSets.Add(referenceType, referenceSet);
+                }
+            }
+            
+            return referenceSet;
+        }
 
     }
 }
