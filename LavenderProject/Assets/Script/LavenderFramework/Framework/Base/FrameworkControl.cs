abfee04f72f8be7a7c17390452f0d1abbe416a7a
八佾舞雩 <852515440@qq.com>
2022-01-26 20:12:07 +0800

    FrameworkBase

---
+++ LavenderProject/Assets/Script/LavenderFramework/Framework/Base/FrameworkControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lavender.Framework
+{
+    public static class FrameworkControl
+    {
+        private static readonly List<FrameworkModule> frameworkModules = new List<FrameworkModule>();
+    }
+}
---
+++ LavenderProject/Assets/Script/LavenderFramework/Framework/Base/FrameworkModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lavender.Framework
+{
+    internal abstract class FrameworkModule
+    {
+        /// <summary>
+        /// 轮询更新
+        /// </summary>
+        /// <param name="elapseSeconds">逻辑时间</param>
+        /// <param name="realElapseSeconds">真实时间</param>
+        internal abstract void Update(float elapseSeconds, float realElapseSeconds);
+
+        /// <summary>
+        /// 关闭游戏模块
+        /// </summary>
+        internal abstract void Shutdown();
+
+    }
+}
